f9fd08cde20e8005587b7939e81fecb54373f889
t2015q <30385071+t2015q@users.noreply.github.com>
2018-08-26 14:48:23 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyA8sgAXwNxUGI-dK2Lz1oG7Srhi4xV0S9I');
+define('APIKEY', '');
 define('GJ_CODE', 'US');
 define('SITE_NAME','gigman');
 define('TITLENAME', 'gitman');
 define('EN2DEKEY','w!A$wYoB0r%:0>&X/5H_3`04>@{pN'');
-define('EMAIL', 'ctgu2015@gmail.com');
+define('EMAIL', '');
 ?>
